f40d4201ce43bb3f11520d8dc4e0b34a5c803178
SimY4 <alex.simkin@outlook.com>
2018-12-05 17:48:47 +1100

    Day 5 solved

---
+++ src/main/scala/AdventOfCode/y2018/Day5.scala
@@ -0,0 +1,29 @@
+package AdventOfCode
+package y2018
+
+import java.text.SimpleDateFormat
+import java.time.{ Duration, Instant, LocalDateTime, ZoneOffset }
+
+import scala.annotation.tailrec
+
+object Day5 {
+
+  val input = """dabAcCaCBAcCcaDA""".stripMargin
+
+  val regex = ('a' to 'z').flatMap { c => Seq(s"$c" + s"${c.toUpper}", s"${c.toUpper}" + s"$c") }.mkString("(", "|", ")").r
+
+  @tailrec def solve0(acc: String): String = {
+    regex.replaceFirstIn(acc, "") match {
+      case res if acc == res => res
+      case res => solve0(res)
+    }
+  }
+
+  def solve(input: String): Int = solve0(input).length
+
+  def solve2(input: String): Int = (for {
+    ch <- ('a' to 'z').par
+    filteredInput = s"[$ch${ch.toUpper}]".r.replaceAllIn(input, "")
+  } yield (solve0(filteredInput).length)).min 
+
+}
