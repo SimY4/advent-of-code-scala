74dc0a5ef1cc5a28cb011c75c149ad49f5b8f485
SimY4 <alex.simkin@outlook.com>
2020-02-26 16:39:44 +1100

    y2015 day17

---
+++ src/main/scala/aoc/y2015/Day17.scala
@@ -0,0 +1,51 @@
+package aoc.y2015
+
+object Day17 with
+  def solve(input: String): Int =
+    val containers = input.linesIterator.map(_.toInt).toList
+    
+    (0 until (1 << containers.size))
+      .count { mask => 
+        (for 
+          (j, i) <- containers.zipWithIndex
+          if (mask & (1 << i)) > 0
+        yield j)
+          .sum == 150
+      }
+
+  def solve2(input: String): Int = 
+    val containers = input.linesIterator.map(_.toInt).toList
+    
+    (0 until (1 << containers.size))
+      .flatMap { mask => 
+        val set = (for 
+          (j, i) <- containers.zipWithIndex
+          if (mask & (1 << i)) > 0
+        yield j)
+        Option.when(set.sum == 150) { set.size }
+      }
+      .groupBy(identity)
+      .toSeq
+      .minBy(_._1)
+      ._2.size
+
+  val input = """33
+                |14
+                |18
+                |20
+                |45
+                |35
+                |16
+                |35
+                |1
+                |13
+                |18
+                |13
+                |50
+                |44
+                |48
+                |6
+                |24
+                |41
+                |30
+                |42""".stripMargin
