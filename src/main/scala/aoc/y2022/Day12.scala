105eebe0a471351eb71a03db7f3987d4bcef96eb
SimY4 <alex.simkin@outlook.com>
2022-12-13 13:10:17 +1100

    y2022 day 12

---
+++ src/main/scala/aoc/y2022/Day12.scala
@@ -0,0 +1,121 @@
+package aoc
+package y2022
+
+import scala.annotation.tailrec
+import scala.collection.mutable
+
+object Day12:
+  @tailrec private def trail(
+    paths: List[List[Coord]]
+  )(grid: Array[Array[Char]], visited: mutable.Set[Coord]): List[Coord] =
+    paths.find(path => grid(path.head) == 'E') match
+      case Some(solution) => solution
+      case None =>
+        paths.flatMap { path =>
+          val coord = path.head
+          val ch    = grid(coord)
+          for
+            neighbour <- coord.neighbours(Direction.hvOnly)
+            if !visited.contains(neighbour)
+            if grid.get(neighbour).exists { nch =>
+              if 'E' == nch then 'z' == ch
+              else ch.toInt + 1 >= nch.toInt
+            }
+          yield {
+            visited.add(neighbour)
+            neighbour :: path
+          }
+        } match
+          case Nil =>
+            paths.map(_.map(grid(_)).mkString)
+            Nil
+          case next => trail(next)(grid, visited)
+
+  def solve(input: String): Int =
+    val grid = input.linesIterator.map(_.toCharArray).toArray
+    val start = grid.zipWithIndex
+      .flatMap((row, i) => row.zipWithIndex.collect { case ('S', j) => Coord(j, i) })
+      .head
+    val end = grid.zipWithIndex
+      .flatMap((row, i) => row.zipWithIndex.collect { case ('E', j) => Coord(j, i) })
+      .head
+    val visited = mutable.HashSet.empty[Coord]
+    visited.add(start)
+
+    trail(for
+      neighbour <- start.neighbours(Direction.hvOnly)
+      if visited.add(neighbour)
+      if grid.get(neighbour).isDefined
+    yield neighbour :: Nil)(grid, visited).size
+
+  def solve2(input: String): Int =
+    val grid = input.linesIterator.map(_.toCharArray).toArray
+    val starts = grid.view.zipWithIndex
+      .flatMap((row, i) =>
+        row.zipWithIndex.collect {
+          case ('S', j) => Coord(j, i)
+          case ('a', j)
+              if Coord(j, i)
+                .neighbours(Direction.hvOnly)
+                .exists(coord => grid.get(coord).exists('b' == _)) =>
+            Coord(j, i)
+        }
+      )
+      .toVector
+    val end = grid.zipWithIndex
+      .flatMap((row, i) => row.zipWithIndex.collect { case ('E', j) => Coord(j, i) })
+      .head
+
+    starts.map { start =>
+      val visited = mutable.HashSet.empty[Coord]
+      visited.add(start)
+
+      trail(for
+        neighbour <- start.neighbours(Direction.hvOnly)
+        if visited.add(neighbour)
+        if grid.get(neighbour).isDefined
+      yield neighbour :: Nil)(grid, visited).size
+    }.min
+
+  val input =
+    """abcccccccccccccccccccccccccccccccaaaaaaaaaaaaaaaaccaaaaaaaaccccccccccccccccccccccccccccccccccccaaaaaa
+      |abcccccccccccccccccccccccccccccccaaaaaaaaaaaaaaaaaccaaaaaaccccccccccccccccccccccccccccccccccccccaaaaa
+      |abcccccccccccccccccccccccccccccccccaaaaaaaacccaaaaccaaaaaaccccccccccccccccccccaaaccccccccccccccccaaaa
+      |abcccccccccccccccccccccccccccccccccccaaaaaaaccaaccccaaaaaaccccccccccccccccccccaaaccccccccccccccccaaaa
+      |abcccccccccccccccccccccccccccccaaacccaaaaaaaacccccccaaccaaccccccccccccccccccccaaaccccccccccccccccaaac
+      |abcccccccccccccccccccccccccccccaaaaaaaaacaaaacccccccccccccccaccaaccccccccccccciiaaccaaaccccccccccaacc
+      |abccccccccaaccccccccccccccccccaaaaaaaaaaccaaacccccccccccccccaaaaaccccccccacaiiiiijjaaaacccccccccccccc
+      |abacccaaccaacccccccccccccccccaaaaaaaaaaccccacccccaaaaccccccccaaaaacccccccaaiiiiijjjjaaaccccccaacccccc
+      |abacccaaaaaacccccccccccccccccaaaaaaaaccccccccccccaaaacccccccaaaaaacccccccaiiiioojjjjjacccaaaaaacccccc
+      |abcccccaaaaaaacccccccccccccccccaaaaaaccccaaccccccaaaacccccccaaaaccccccccciiinnoooojjjjcccaaaaaaaccccc
+      |abccccccaaaaaccccccccccccccccccaaaaaacccaaaaccccccaaacccccccccaaaccccccchiinnnooooojjjjcccaaaaaaacccc
+      |abcccccaaaaacccccccccccccccccccaacccccccaaaaccccccccccccccccccccccccccchhiinnnuuoooojjjjkcaaaaaaacccc
+      |abccccaaacaaccccccccccccccccccccccccccccaaaaccccccccccccccccccaaacccchhhhhnnntuuuoooojjkkkkaaaacccccc
+      |abccccccccaacccccccccccccccccccccccccccccccccccccccccccccccccccaacchhhhhhnnnnttuuuuoookkkkkkkaacccccc
+      |abcccccccccccccccccccaacaaccccccccccccccccccccccccccccccccccaacaahhhhhhnnnnntttxuuuoopppppkkkkacccccc
+      |abcccccccccccccccccccaaaaacccccccccaccccccccccccccccccccccccaaaaahhhhmnnnnntttxxxuuupppppppkkkccccccc
+      |abccccccccccccccccccccaaaaacccccaaaacccccccccccccccccccccccccaaaghhhmmmmttttttxxxxuuuuuupppkkkccccccc
+      |abcccccccccccccccccccaaaaaaaccccaaaaaaccccccccccccccccccccccccaagggmmmmtttttttxxxxuuuuuuvppkkkccccccc
+      |abcccccccccccccccccccaaaaaaaaaaacaaaaacccccccccccccccccccccccaaagggmmmttttxxxxxxxyyyyyvvvppkkkccccccc
+      |abccccccccccccccccccccaaaaaaaaaaaaaaaccccccccccccccccccccaacaaaagggmmmtttxxxxxxxyyyyyyvvppplllccccccc
+      |SbcccccccccccccccccccaaaaaaaaaacaccaaccccccccccccccccccccaaaaaccgggmmmsssxxxxEzzzyyyyvvvpplllcccccccc
+      |abcccccccccccccccccccccaaaaaaccccccccccccccaacaaccccccccaaaaaccccgggmmmsssxxxxyyyyyvvvvqqplllcccccccc
+      |abccccccccccccccccccccccaaaaaacccccccccccccaaaacccccccccaaaaaacccgggmmmmsssswwyyyyyvvvqqqlllccccccccc
+      |abcccccccccccccccccccccaaaaaaaccccccccccccaaaaacccccccccccaaaaccccgggmmmmsswwyyyyyyyvvqqllllccccccccc
+      |abcccccccccccccccccccccaaaccaaacccccccccccaaaaaaccccccccccaccccccccgggooosswwwywwyyyvvqqlllcccccccccc
+      |abccccccccccccccccccccccacccccccccccccccccacaaaacccccccccccccccccccfffooosswwwwwwwwvvvqqqllcccccccccc
+      |abccccccccccccccccccccccccccccccccccccccccccaacccccccccccccccccccccfffooosswwwwwrwwvvvqqqllcccccccccc
+      |abccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccffooossswwwrrrwvvvqqqmmcccccccccc
+      |abccccaaacccccccccccccccccccccccccccccccccccccccccccccccccccccccccccffooosssrrrrrrrrqqqqmmmcccccccccc
+      |abccccaaacaacccccaaccccaaaacccccccccccccccccccccccccccccccccccccccccffooossrrrrrnrrrqqqqmmmcccaaacccc
+      |abcccccaaaaaccaaaaacccaaaaacccccccccccccccccccccccccccccccccccccccccfffoooorrnnnnnnmqqmmmmmcccaaacccc
+      |abccaaaaaaaacccaaaaaccaaaaaaccccccccccccccccccccccccccccccccccccccccfffooonnnnnnnnnmmmmmmmcccaaaccccc
+      |abcccaaaaacccccaaaaaccaaaaaaccccccaacccccccccccccccccccccccccccccccccfffoonnnnneddnmmmmmmccccaaaccccc
+      |abccccaaaaacccaaaaacccaaaaaacccccaaaaaaccccccccccccccccccccaaccccccccffeeeeeeeeeddddddddccccaaaaccccc
+      |abccccaacaaacccccaacccccaacccccccaaaaaaaccccccccccccccccaaaaaccccccccceeeeeeeeeedddddddddccaccaaccccc
+      |abccccaacccccccccccccccccccccccccaaaaaaaccaaaccccccccccccaaaaaccccccccceeeeeeeeaaaaddddddcccccccccccc
+      |abcccccccccccaaccccccccccccccccccccccaaaaaaaaacccccccccccaaaaacccccccccccccaaaacaaaacccccccccccccccaa
+      |abccccccccaacaaacccccccccccccccccccccaaaaaaaacccccccccccaaaaaccccccccccccccaaaccaaaaccccccccccccccaaa
+      |abccccccccaaaaacccccccccccccccccccccacaaaaaaccccccccccccccaaacccccccccccccccaccccaaacccccccccccacaaaa
+      |abcccccccccaaaaaaccccccccccccccccaaaaaaaaaaacccccccccccccccccccccccccccccccccccccccacccccccccccaaaaaa
+      |abcccccccaaaaaaaaccccccccccccccccaaaaaaaaaaaaacccccccccccccccccccccccccccccccccccccccccccccccccaaaaaa""".stripMargin
