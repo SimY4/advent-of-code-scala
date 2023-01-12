e3eb4514e0a75f660a2238e847a3b4b1988a8d0e
SimY4 <alex.simkin@outlook.com>
2023-01-12 19:04:59 +1100

    y2017 day17

---
+++ src/main/scala/aoc/y2017/Day17.scala
@@ -0,0 +1,23 @@
+package aoc.y2017
+
+import scala.collection.mutable
+
+object Day17:
+  def solve(input: Int): Int =
+    val buffer = mutable.ArrayBuffer(0)
+    val finalPos = (1 to 2017).foldLeft(0) { (pos, i) =>
+      val next = (pos + input) % buffer.size + 1
+      buffer.insert(next, i)
+      next
+    }
+    buffer((finalPos + 1) % buffer.size)
+
+  def solve2(input: Int): Int =
+    val (_, valueAt) = (1 to 50000000).foldLeft(0 -> 0) { case ((pos, valueAt), i) =>
+      val next = (pos + input) % i + 1
+      if next == 1 then next -> i
+      else next              -> valueAt
+    }
+    valueAt
+
+  val input = 370
