921624d443b753d861d3999864d892868ed16da8
SimY4 <alex.simkin@outlook.com>
2023-12-06 21:51:59 +1000

    y2023 day6

---
+++ src/main/scala/aoc/y2023/Day6.scala
@@ -0,0 +1,16 @@
+package aoc.y2023
+
+object Day6:
+  def solve(input: List[(Int, Int)]): Int =
+    (for
+      (time, distance) <- input
+      wins = (0 until time).count(t => t * (time - t) > distance)
+    yield wins).product
+
+  def solve2(input: List[(Int, Int)]): Int =
+    val (time, distance) = input.foldRight(("", "")):
+      case ((t, d), (at, ad)) => (t.toString + at, d.toString() + ad)
+
+    (0L until time.toInt).count(t => t * (time.toInt - t) > distance.toInt)
+
+  val input = List((41, 244), (66, 1047), (72, 1228), (66, 1040))
